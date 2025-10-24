4f73aa230837805fb6f69184a8e14b9188f43a6c
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-10-24 15:58:31 +0900

    08_removeFromArray: Pass all specs

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targets) {
+    return array.filter( (element) => {
+        return !targets.includes(element);
+    });
 };
 
 // Do not edit below this line
