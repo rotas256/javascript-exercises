f9fadda4d1ff37e5f6f62fca871c15db4262dfc2
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-10-24 14:47:16 +0900

    07_reverseString: use Array.prototype.reverse()

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,14 +1,15 @@
 const reverseString = function(string) {
     
-    if( string === ''){
-        return string;
-    }
-    const length = string.length;
-    let reverseStr = '';
-    for( let i=0; i<length; i++ ){
-        reverseStr += string[length-1-i];
-    }
-    return reverseStr;
+    // if( string === ''){
+    //     return string;
+    // }
+    // const length = string.length;
+    // let reverseStr = '';
+    // for( let i=0; i<length; i++ ){
+    //     reverseStr += string[length-1-i];
+    // }
+    // return reverseStr;
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
