9ff97687edcfe7bd6a11874d903733239d54243d
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-10-24 16:10:30 +0900

    09_sumAll: Pass all specs

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if( num1 < 0 || num2 < 0 ) return "ERROR";
+    else if( !Number.isInteger(num1) || !Number.isInteger(num2) ) return "ERROR";
+    let start, end;
+    if( num1 < num2){
+        start = num1;
+        end = num2;
+    }else{
+        start = num2;
+        end = num1;
+    }
+    if( start < 0 ) return "ERROR";
 
+    let sum = 0;
+    for( let i=start; i<=end; i++ ){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
