8675f1e8214f74e6a5461a9fbb2a5397fc409e56
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-10-24 14:43:34 +0900

    07_reverseString: Pass all specs

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    if( string === ''){
+        return string;
+    }
+    const length = string.length;
+    let reverseStr = '';
+    for( let i=0; i<length; i++ ){
+        reverseStr += string[length-1-i];
+    }
+    return reverseStr;
 };
 
 // Do not edit below this line
