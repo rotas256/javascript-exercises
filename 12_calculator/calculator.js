29bbc8f180d484f5bf930963c15425a016ee2045
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-11-13 16:52:39 +0900

    12_calculator: Pass all specs

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce( (sum, currentNum) => sum + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce( (multiply, currentNum) => multiply * currentNum, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  // let pow = 1;
+	// while(exponent > 0){
+  //   pow *= base;
+  //   exponent--;
+  // }
+  // return pow;
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let fact = 1;
+  for( let i=number; i>0; i-- ){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
