9868de44ec2ad14d3d96855e2202d6b87786ef45
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-11-14 11:25:17 +0900

    13_palindromes: Pass all specs

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let chars = string.replaceAll(/[^a-z0-9]/gi, '').toLowerCase();
+    // let length = chars.length;
+    // for( let i=0; i<length/2; i++ ){
+    //     if( chars.charAt(i) !== chars.charAt(length-1-i) ) {
+    //         return false;
+    //     }
+    // }
+    // return true;
 
+    let reverseChars = chars.split('').reverse().join('');
+    return chars === reverseChars;
 };
 
 // Do not edit below this line
