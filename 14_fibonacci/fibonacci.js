57a7f45fbf2994a7789fad467f77f0063a6abd49
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-11-14 11:40:26 +0900

    15_fibonacci: Pass all specs

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const number = parseInt(index);
+    if(number < 0) return "OOPS";
+    const fib = [0, 1];
+    for( let i=2; i<=number; i++){
+        fib[i] = fib[i-2] + fib[i-1];
+    }
+    return fib[number];
 };
 
 // Do not edit below this line
