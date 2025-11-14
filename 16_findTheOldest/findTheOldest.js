7147c1f289b6aefc5734fe1c8fd6d1a543b7b75c
rotas256 <97214237+rotas256@users.noreply.github.com>
2025-11-14 12:18:12 +0900

    16_findTheOldest: Pass all specs

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const calcAge = (person) => {
+        const CurrentYear = 2025;
+        if( person.yearOfDeath ) return person.yearOfDeath - person.yearOfBirth;
+        else return CurrentYear - person.yearOfBirth;
+    };
+    return oldestPerson = people.reduce((oldest, person) => {
+        if( !oldest ) return person;
+        if( calcAge(oldest) >= calcAge(person) ) return oldest;
+        else return person;
+    }, {});
 };
 
 // Do not edit below this line
